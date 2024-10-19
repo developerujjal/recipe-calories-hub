772be7a148360743b3889ac623b43fd4fda5009b
Developer Ujjal <ujjalchandra213@gmail.com>
2024-10-20 05:35:23 +0600

    almost completed

--- src/components/Cooking/Cooking.jsx
+++ src/components/Cooking/Cooking.jsx
@@ -1,11 +1,14 @@
+import PropTypes from 'prop-types';
 import './Cooking.css'
 
-const Cooking = () => {
+const Cooking = ({ cook, handlePreparing, cooking }) => {
+
+
     return (
         <div className="cook-and-cooking-wrapper">
             <div className="cook-for-preparing-main">
                 <div className='cook-heading'>
-                    <h3>Want to cook: <span>0</span></h3>
+                    <h3>Want to cook: <span>{cook.length}</span></h3>
                 </div>
                 <table className='main-table'>
                     <thead>
@@ -18,21 +21,66 @@ const Cooking = () => {
                         </tr>
                     </thead>
                     <tbody className='tbody-main'>
-                        <tr>
-                            <td>1</td>
-                            <td>Chicken Caesar Salad</td>
-                            <td><span>20</span> minutes</td>
-                            <td><span>400</span> calories</td>
-                            <td><button className="status-button">Preparing</button></td>
-                        </tr>
+
+                        {
+                            cook.map((elementCook, index) => (
+                                <tr key={elementCook.id}>
+                                    <td>{index + 1}</td>
+                                    <td>{elementCook.recipe_name}</td>
+                                    <td><span>{elementCook.preparing_time}</span> minutes</td>
+                                    <td><span>{elementCook.calories}</span> calories</td>
+                                    <td><button onClick={() => handlePreparing(elementCook, elementCook.id)} className="status-button">Preparing</button></td>
+                                </tr>
+                            ))
+                        }
+
                     </tbody>
                 </table>
             </div>
-            <div className="current-cooking-main">
 
+            <div className="cook-for-preparing-main">
+                <div className='cook-heading'>
+                    <h3>Currently Cooking: <span>{cooking.length}</span></h3>
+                </div>
+                <table className='main-table'>
+                    <thead>
+                        <tr id='main-table-tr'>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Time</th>
+                            <th>Calories</th>
+                        </tr>
+                    </thead>
+                    <tbody className='tbody-main'>
+
+                        {
+                            cooking.map((elementCooking, index) => (
+                                <tr key={index} id='tbody-cooking-tr'>
+                                    <td>{index + 1}</td>
+                                    <td>{elementCooking.recipe_name}</td>
+                                    <td><span>{elementCooking.preparing_time}</span> minutes</td>
+                                    <td><span>{elementCooking.calories}</span> calories</td>
+
+                                </tr>
+                            ))
+                        }
+
+                    </tbody>
+                </table>
+
+                <div className='totals'>
+                    <p>Total Time = <span>0</span> minutes</p>
+                    <p>Total Calories = <span>0</span> calories</p>
+                </div>
             </div>
         </div>
     );
 };
+
+Cooking.propTypes = {
+    cook: PropTypes.array.isRequired,
+    handlePreparing: PropTypes.func.isRequired,
+    cooking: PropTypes.array.isRequired
+}
 
 export default Cooking;
--- src/components/RecipeSection/RecipeSection.jsx
+++ src/components/RecipeSection/RecipeSection.jsx
@@ -8,12 +8,38 @@ import './RecipeSection.css'
 const RecipeSection = () => {
 
     const [recipes, setRecipes]= useState([])
+    const [cook, setCook]= useState([])
+    const [cooking, setCooking]= useState([])
 
     useEffect(()=>{
         fetch('./recipesData.json')
         .then(res => res.json())
         .then(data => setRecipes(data))
     },[])
+
+    const handleCookBtn = (recipe)=>{
+        const isExist = cook.find((singelCook)=> singelCook.id === recipe.id);
+        if(!isExist){
+            const newCook = [...cook, recipe]
+            setCook(newCook)
+        }
+
+        else{
+            alert("You already have Select, Plz try another one!")
+        }
+    }
+
+
+    const handlePreparing = (elementCook, id)=>{
+        
+        const newCook = cook.filter((iteamCook)=> iteamCook.id !== id);
+        setCook(newCook)
+
+        const newCooking = [...cooking, elementCook];
+        setCooking(newCooking)
+    }
+
+    console.log(cooking)
 
     return (
         <div className="recipe-section-wrapper">
@@ -24,12 +50,18 @@ const RecipeSection = () => {
             <div className='all-recipes-and-cook'>
                 <div className='all-recipes'>
                     {
-                        recipes.map((recipe)=> <RecipeCard recipe={recipe} key={recipe.id}></RecipeCard>)
+                        recipes.map((recipe)=> <RecipeCard
+                        recipe={recipe}
+                        handleCookBtn={handleCookBtn}
+                        key={recipe.id}></RecipeCard>)
                     }
                 </div>
                 <div className='recipes-cook'>
                    {
-                    <Cooking></Cooking>
+                    <Cooking
+                    cook={cook}
+                    cooking={cooking}
+                    handlePreparing={handlePreparing}></Cooking>
                    }
                 </div>
             </div>
